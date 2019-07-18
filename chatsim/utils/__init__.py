e9c90d8611f28ae6b4e976d88c37b9f2a974f20b
Mansour Saffar M <msaffarmehrjardy@gmail.com>
2019-07-18 17:55:20 -0600

    Add init file for utils

---
+++ chatsim/utils/__init__.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+from .common import Annotation, Goal, UserGoal, DiagAct
+
+CURRENT_DIR = Path(os.path.dirname(__file__))
+DATA_DIR = CURRENT_DIR / '../../data'
+
